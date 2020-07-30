f22c972fb916890b0eb0510d6f16653bcf36ef02
Gonzalo Betancourt <gonzalobetancourt@me.com>
2020-07-30 01:53:43 -0500

    Results List loads correctly fixed spelling bug results__list

--- recipe/src/js/views/searchViews.js
+++ recipe/src/js/views/searchViews.js
@@ -5,12 +5,26 @@ export const getInput = () => elements.searchInput.value;
 
 // Clear  search field
 export const clearInput = () => {
-    elements.searchInput.value = "";
+    elements.searchInput.value = '';
 };
 
 // Clear left side results list
 export const clearResultsList = () => {
-    elements.searchResultList.innerHTML = "";
+    elements.searchResultList.innerHTML = '';
+}
+
+const limitRecipeTitle = (title, limit = 17) => {
+    const newTitle = [];
+    if (title.length > limit) {
+        title.split(" ").reduce((accumulator, current) => {
+            if (accumulator + current.length <= limit) {
+                newTitle.push(current);
+            }
+            return accumulator + current.length;
+        }, 0);
+        return `${newTitle.join(' ')}...`;
+    }
+    return title;
 }
 
 // function to add items to list
@@ -22,7 +36,7 @@ const renderRecipe = recipe => {
                     <img src="${recipe.image_url}" alt="${recipe.title}">
                 </figure>
                 <div class="results__data">
-                    <h4 class="results__name">${recipe.title}</h4>
+                    <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
                     <p class="results__author">${recipe.publisher}</p>
                 </div>
             </a>
