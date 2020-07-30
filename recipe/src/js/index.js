2663ea63d7e3e8918833a5ff7fef950a9177becf
Gonzalo Betancourt <gonzalobetancourt@me.com>
2020-07-29 20:55:50 -0500

    async function to request query

--- recipe/src/js/index.js
+++ recipe/src/js/index.js
@@ -1,6 +1,33 @@
-import string from './models/Search';
+import axios from 'axios'; // works with all browsers
 
-//import { add as a , multiply as mul, ID } from './views/searchViews';
-import * as searchView from './views/searchViews';
+async function getResults(query) {
+    // const proxy = 'https://cors-anywhere.herokuapp.com'
+    // const key = ''; // I dont need a key with thi API
+    try {
+        const result = await axios(`https://forkify-api.herokuapp.com/api/search?q=${query}`);
+        const recipes = result.data.recipes
+        console.log(recipes);
+    } catch (error) {
+        alert(error)
+    }
+}
 
-console.log(`Using imported functions. ${searchView.add(searchView.ID, 2)} and ${searchView.multiply(3, 5)}. ${string}`);
+getResults("tacos");
+
+
+// https://forkify-api.herokuapp.com/api/search?q=pizza
+
+// Currently I am going to build the app with a free API that does not use proxy or key
+
+
+
+
+
+
+// TEST CODE
+// import string from './models/Search';
+
+// //import { add as a , multiply as mul, ID } from './views/searchViews';
+// import * as searchView from './views/searchViews';
+
+// console.log(`Using imported functions. ${searchView.add(searchView.ID, 2)} and ${searchView.multiply(3, 5)}. ${string}`);
